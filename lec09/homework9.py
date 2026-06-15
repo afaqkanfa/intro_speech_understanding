e6c2456a3dea4ba022ce3db2ad8e30887650620c
afaqkanfa <afaqkanfa@gmail.com>
2026-06-15 02:50:40 +0100

    Update homework9.py
    Implemented VAD, model creation, and speech recognition using cosine similarity according to Homework 9 requirements.

--- lec09/homework9.py
+++ lec09/homework9.py
@@ -13,7 +13,23 @@ def VAD(waveform, Fs):
     segments (list of arrays) - list of the waveform segments where energy is 
        greater than 10% of maximum energy
     '''
-    raise RuntimeError("You need to change this part")
+    frame_length = int(0.025 * Fs)
+step = int(0.01 * Fs)
+
+frames = np.array([
+    waveform[m:m+frame_length]
+    for m in range(0, len(waveform)-frame_length, step)
+])
+
+energy = np.sum(np.square(frames), axis=1)
+vad = np.array([1 if e > 0.1*np.max(energy) else 0 for e in energy])
+
+start = [m*step for m in range(1,len(vad)) if vad[m-1]==0 and vad[m]==1]
+end = [m*step for m in range(1,len(vad)) if vad[m-1]==1 and vad[m]==0]
+
+segments = [waveform[start[k]:end[k]] for k in range(len(start))]
+
+return segments
 
 def segments_to_models(segments, Fs):
     '''
@@ -29,7 +45,24 @@ def segments_to_models(segments, Fs):
     @returns:
     models (list of arrays) - average log spectra of pre-emphasized waveform segments
     '''
-    raise RuntimeError("You need to change this part")
+    models = []
+
+N = int(0.004 * Fs)
+S = int(0.002 * Fs)
+
+for x in segments:
+    frames = np.array([
+        x[m+1:m+1+N] - x[m:m+N]
+        for m in range(0, len(x)-N, S)
+    ])
+
+    mstft = np.abs(np.fft.fft(frames, axis=1))
+    sgram = 20*np.log10(np.maximum(0.01*np.amax(mstft), mstft))
+
+    model = np.average(sgram[:,0:int(N/2)], axis=0)
+    models.append(model)
+
+return models
 
 def recognize_speech(testspeech, Fs, models, labels):
     '''
@@ -47,6 +80,26 @@ def recognize_speech(testspeech, Fs, models, labels):
     sims (Y-by-K array) - cosine similarity of each model to each test segment
     test_outputs (list of strings) - recognized label of each test segment
     '''
-    raise RuntimeError("You need to change this part")
+    testsegments = VAD(testspeech, Fs)
+testspectra = segments_to_models(testsegments, Fs)
+
+sims = np.zeros((len(models), len(testspectra)))
+
+for y in range(len(models)):
+    for k in range(len(testspectra)):
+        x = testspectra[k]
+        m = models[y]
+
+        sims[y,k] = np.dot(x,m) / (
+            np.sqrt(np.sum(np.square(x))) *
+            np.sqrt(np.sum(np.square(m)))
+        )
+
+test_outputs = [
+    labels[np.argmax(sims[:,k])]
+    for k in range(len(testspectra))
+]
+
+return sims, test_outputs
 
 
